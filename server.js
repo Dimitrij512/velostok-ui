f1f88b269e8428b17578b1da53857dd8fe7dead7
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-07-07 02:02:48 +0300

    3test server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + 'velostok-ui'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  index = path.join(__dirname, 'build', 'index.html');
+  index = path.join(__dirname, '.', 'index.html');
   res.sendFile(index);
 });
 // Start the app by listening on the default Heroku port
