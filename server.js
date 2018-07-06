839b27915d575b0f7cef307b1cfde8b050b989df
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-07-06 23:24:45 +0300

    minor changes for deploy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
+express = require('express');
+app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
