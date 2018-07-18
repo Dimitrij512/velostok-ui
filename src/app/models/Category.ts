4d718da3d9292b17ab16f4b14d4379f050fb1c0d
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-07-18 23:16:00 +0300

    changed model category

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,7 +1,10 @@
+import {SubCategory} from "./SubCategory";
+
 export class Category {
   id: number;
   name: string;
   description: string;
   image: string;
   path: string;
+  subCategories: Array<SubCategory>;
 }
