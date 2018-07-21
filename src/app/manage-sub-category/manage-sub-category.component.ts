1768e47238d229ff13c7e21f399d7e5bc2768f8d
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-07-21 16:14:12 +0300

    added manage sub categories and dialog window for create/update them

--- src/app/manage-sub-category/manage-sub-category.component.ts
+++ src/app/manage-sub-category/manage-sub-category.component.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatDialog, MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
+import {Category} from "../models/Category";
+import {DialogAdminCategoryComponent} from "../dialog-admin-category/dialog-admin-category.component";
+import {DialogConfirmDeleteComponent} from "../dialog-confirm-delete/dialog-confirm-delete.component";
+import {SubCategory} from "../models/SubCategory";
+import {DialogAdminSubCategoryComponent} from "../dialog-admin-sub-category/dialog-admin-sub-category.component";
+import {SubCategoryService} from "../services/subCategory-service";
 
 @Component({
   selector: 'app-manage-sub-category',
   templateUrl: './manage-sub-category.component.html',
-  styleUrls: ['./manage-sub-category.component.css']
+  styleUrls: ['./manage-sub-category.component.css'],
+  providers:[SubCategoryService]
 })
 export class ManageSubCategoryComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  displayedColumns = ['name', 'categoryName', 'edit', 'delete'];
+  subCategories: Array<SubCategory>;
+  dataSource: any;
+  pageSizeOptions = [5, 10, 25, 50];
+  pageSize: Number;
+  length:Number;
+  positionTollTip = "above";
+
+
+  constructor(public subCategoryService: SubCategoryService, public dialog: MatDialog, public dialogConfirm: MatDialog) {
+
+  }
 
   ngOnInit() {
+    this.subCategoryService.getAllSubCategories().subscribe(data => this.dataHandler(data), this.searchErrorHandler);
   }
+
+  public dataHandler(categories: any) {
+    this.subCategories = categories as Array<SubCategory>;
+    this.dataSource = new MatTableDataSource(this.subCategories);
+    this.dataSource.sort = this.sort;
+
+    this.dataSource.paginator = this.paginator;
+    this.paginator._intl.itemsPerPageLabel = 'Кількість елементів на сторінці';
+    this.paginator._intl.nextPageLabel = 'Наступна сторінка';
+    this.paginator._intl.previousPageLabel = 'Попердня сторінка';
+    this.pageSize = 5;
+    this.length = this.subCategories.length;
+  }
+
+  createSubCategory() {
+    const dialogRef = this.dialog.open(DialogAdminSubCategoryComponent, {
+      data: new Category(),
+      minHeight: '30%',
+      minWidth: '40%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        this.subCategories.push(result);
+        this.dataHandler(this.subCategories.reverse());
+      }
+    });
+  };
+
+  updateSubCategory(row) {
+    const dialogRef = this.dialog.open(DialogAdminSubCategoryComponent, {
+      data: row,
+      minHeight: '30%',
+      minWidth: '40%'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        let index = this.subCategories.findIndex(category => category.id == result.id);
+        this.subCategories.splice(index, 1);
+        this.subCategories.splice(index, 0, result);
+        this.dataHandler(this.subCategories);
+      }
+    });
+  }
+
+  openDialogConfirmRemove(row): void {
+    let dialogRef = this.dialogConfirm.open(DialogConfirmDeleteComponent, {
+      width: '500px',
+      minWidth:'500px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      result == 'confirm' ? this.deleteCategory(row) : 'doNothing';
+    });
+  }
+
+  deleteCategory(row) {
+    console.log(row);
+    for (let curCategory = 0; curCategory < this.subCategories.length; curCategory++) {
+      if (row.id === this.subCategories[curCategory].id) {
+        this.subCategories.splice(curCategory, 1);
+      }
+    }
+
+    this.subCategoryService.deleteSubCategory(row.id);
+    this.dataHandler(this.subCategories);
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  public searchErrorHandler(error: any) {
+    alert("Вході виконання програми виникла помилка, спробуйте пізніше");
+  }
+
 
 }
---
+++ src/app/services/subCategory-service.ts
@@ -0,0 +1,36 @@
+import {SubCategory} from "../models/SubCategory";
+import {HttpClient} from "@angular/common/http";
+import {BASEURL} from "../constants/projectsConstants";
+import {SuperCategory} from "../models/SuperCategory";
+import {Observable, of} from "rxjs/index";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class SubCategoryService {
+
+  public subCategories: Observable<SubCategory[]> = of([]);
+
+  constructor(private http: HttpClient) {}
+
+  getAllSubCategories(): Observable<SubCategory[]> {
+    this.subCategories =  this.http.get<SubCategory[]>(BASEURL + "/allSubCategories");
+    return this.subCategories;
+  }
+
+  createSubCategory(superCategory: SuperCategory): Observable<any> {
+    return this.http.post(BASEURL + "/admin/subCategory", superCategory);
+  }
+
+  updateSubCategory(superCategory: SuperCategory): Observable<any> {
+    return this.http.put(BASEURL + "/admin/subCategory", superCategory);
+  }
+
+  deleteSubCategory(id: String) {
+    this.http.delete(BASEURL + "/admin/subCategory/" + id).toPromise().catch(this.handleHttpError);
+  }
+
+  private handleHttpError(error: any) {
+    alert("Щось пішло не так, спробуйте ще раз або зверніться до адміністратора. Помилка : " + error);
+  }
+
+}
