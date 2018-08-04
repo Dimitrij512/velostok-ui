ad8aadddcc29fd4594161be054dd98f4ea654f7b
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-08-04 23:16:21 +0300

    added https

--- src/app/constants/projectsConstants.ts
+++ src/app/constants/projectsConstants.ts
@@ -1,3 +1,3 @@
 
 export const BASEURL: string = 'https://velostok.herokuapp.com/';
-export const CURRENCY_URL:string = "http://free.currencyconverterapi.com/api/v5/convert?q=USD_UAH&compact=y";
+export const CURRENCY_URL:string = "https://free.currencyconverterapi.com/api/v5/convert?q=USD_UAH&compact=y";
