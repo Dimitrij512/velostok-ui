699342ab45f99d82f6ed5a72341fe5b0964d3dbf
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-11-08 20:51:55 +0200

    added shared service

---
+++ src/app/services/sharedService.ts
@@ -0,0 +1,8 @@
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+
+@Injectable()
+export class SharedService {
+  public IsUserLoggedIn: Subject<boolean> = new Subject<boolean>();
+  public IsUserAdmin: Subject<boolean> = new Subject<boolean>();
+}
