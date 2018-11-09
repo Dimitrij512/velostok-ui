56d570de3036480fb816ff15c8732bf787aeea43
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-11-09 08:26:54 +0200

    changed url for production

--- src/app/constants/projectsConstants.ts
+++ src/app/constants/projectsConstants.ts
@@ -1,6 +1,6 @@
 
-//export const BASEURL: string = 'https://velostok.herokuapp.com/';
-export const BASEURL: string = 'http://localhost:8082';
+export const BASEURL: string = 'https://velostok.herokuapp.com/';
+//export const BASEURL: string = 'http://localhost:8082';
 export const CURRENCY_URL:string = "https://free.currencyconverterapi.com/api/v5/convert?q=USD_UAH&compact=y";
 export const ID: string = "currentUserId";
 export const LOGIN: string = 'currentUserLogin';
